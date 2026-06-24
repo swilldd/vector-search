ef7819d5fcc52c269c511b2294de086fb8e38482
Shanice <30316866+swilldd@users.noreply.github.com>
2026-06-24 10:23:28 +0100

    completed module 2 content. implemented vector search and rag with sqlitesearch and pgvector

--- rag_helper.py
+++ rag_helper.py
@@ -96,3 +96,37 @@ class VectorRAGBase(RAGBase):
             num_results=num_results,
             filter_dict=filter_dict
         )
+
+class PGVectorRAGBase(RAGBase):
+
+    def __init__(self, embedder, conn, **kwargs):
+        super().__init__(index=None, **kwargs)
+        self.embedder = embedder
+        self.conn = conn
+
+    def vec_to_str(self, vector):
+        return '[' + ','.join(str(x) for x in vector) + ']'
+
+    def search(self, query, num_results=5):
+        query_vector = self.embedder.encode(query)
+        query_str = self.vec_to_str(query_vector)
+
+        rows = self.conn.execute(
+            """
+            SELECT course, section, question, answer
+            FROM documents
+            WHERE course = %s
+            ORDER BY embedding <=> %s::vector
+            LIMIT %s
+            """,
+            (self.course, query_str, num_results)
+        ).fetchall()
+
+        return [
+            {
+                'course': r[0], 
+                'section': r[1], 
+                'question': r[2], 
+                'answer': r[3], 
+                } for r in rows
+        ]
